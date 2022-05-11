462b3e92a52d7ede900d5f0c834b61ef137afeab
mauro-mauro <para_mauro@hotmail.com>
2022-05-11 17:36:28 -0300

    refresh token OK

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -22,4 +22,8 @@ export class AuthService {
   public login(loginUsuario: LoginUsuario):Observable<JwtDto>{
     return this.httpClient.post<JwtDto>(this.authURL + 'login', loginUsuario);
   }
+
+  public refresh(dto: JwtDto):Observable<JwtDto>{
+    return this.httpClient.post<JwtDto>(this.authURL + 'refresh', dto);
+  }
 }
